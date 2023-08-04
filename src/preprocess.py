63d0911fcd4cc8acc4974ab264f712b50dd17802
Dowonida <100022949+Dowonida@users.noreply.github.com>
2023-08-04 07:06:32 +0000

    preproc1

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,22 +1,27 @@
 import numpy as np
 import pandas as pd
-# from category_encoders import TargetEncoder
+from sklearn.preprocessing import OneHotEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import FunctionTransformer
 
-np
-pd
+
 # TargetEncoder
 
-CAT_FEATURES = [
-]
+CAT_FEATURES = ["weather","season"]
 
-def func1():
-    return
+def func1(df):
+    weather={1:'clear',2:'mist',3:'light',4:'heavy'}
+    season={ 1 : 'spring', 2 : 'summer', 3 : 'fall', 4 : 'winter'} 
+    df['weather'] = df.apply(lambda x: weather[int(x)])
+    df['season'] = df.apply(lambda x: season[int(x)])
+    return df
 
 preprocess_pipeline = ColumnTransformer(
     transformers = [
-        ("transformation1", FunctionTransformer(func1), ["col1"]),
+        ("transformation1", FunctionTransformer(func1), CAT_FEATURES),
+        ("one_hot", OneHotEncoder(), CAT_FEATURES),
+        ("count_log", FunctionTransformer(np.log), ['count']),
+
         # ("target_encoding", TargetEncoder(), CAT_FEATURES),
     ]
 )

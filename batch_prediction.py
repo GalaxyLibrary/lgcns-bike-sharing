7958264bc6265f64c0cd452f144821fc32be357e
KeunhoLee <33661797+KeunhoLee@users.noreply.github.com>
2023-08-04 07:07:19 +0000

    Modify batch_prediction.py for test run.

--- batch_prediction.py
+++ batch_prediction.py
@@ -1,1 +1,45 @@
-# Write down here.
+import os
+import sys
+import warnings
+from datetime import datetime
+
+import joblib
+import numpy as np
+import pandas as pd
+
+from src.common.constants import (
+    ARTIFACT_PATH,
+    DATA_PATH,
+    LOG_FILEPATH,
+    PREDICTION_PATH,
+)
+from src.common.logger import handle_exception, set_logger
+
+logger = set_logger(os.path.join(LOG_FILEPATH, "logs.log"))
+sys.excepthook = handle_exception
+warnings.filterwarnings(action="ignore")
+
+if __name__ == "__main__":
+    DATE = datetime.now().strftime("%Y%m%d")
+    test = pd.read_csv(os.path.join(DATA_PATH, "bike_sharing_test.csv"))
+    model = joblib.load(os.path.join(ARTIFACT_PATH, "model.pkl"))
+
+    X = test.drop(["datetime", "count"], axis=1, inplace=False)
+    id_ = test["datetime"].to_numpy()
+    y = test["count"].to_numpy()
+
+    # 테스트 데이터에 대한 피처 데이터 저장
+    # model["preprocessor"].transform(X=X).to_csv(
+    #     os.path.join(DATA_PATH, "storage", "bike_sharing_test_feature.csv"),
+    #     index=False,
+    # )
+
+    pred_df = pd.DataFrame({"datetime": id_, "count": np.square(model.predict(X))})
+
+    logger.info(f"Batch prediction for {len(pred_df)} users is created.")
+
+    save_path = os.path.join(PREDICTION_PATH, f"{DATE}_rent_prediction.csv")
+    pred_df.to_csv(save_path, index=False)
+    logger.info(
+        "Prediction can be found in the following path:\n" f"{save_path}"
+    )
